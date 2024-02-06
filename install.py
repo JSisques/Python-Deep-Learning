185999dd43a2a995e376f9eb92a7fb38b2403cdd
Javier Plaza Sisqués <javierplazasisques@gmail.com>
2024-02-06 14:55:14 +0100

    Update

--- install.py
+++ install.py
@@ -1,3 +1,7 @@
 import os
+
+os.system("wsl --install")
+os.system("python -m venv venv")
+os.system("source venv/bin/activate")
 os.system("python.exe -m pip install --upgrade pip")
 os.system("pip install -r requirements.txt")
